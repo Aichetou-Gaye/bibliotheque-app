f27435c536b2009787e79218de16b2f2418c3e88
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-08-29 01:41:29 +0100

    Add updates

--- src/components/livres/ListeLivre.vue
+++ src/components/livres/ListeLivre.vue
@@ -39,7 +39,7 @@
                         <h5 class="modal-title text-danger" id="exampleModalLabel">Informations du livre</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <div class="modal-body">
+                    <div class="modal-body">    
                         <p>ISBN : {{ livreShow?.isbn }}</p>
                         <p>Titre: {{ livreShow?.title }}</p>
                         <p>Auteur : {{ livreShow?.author }}</p>
